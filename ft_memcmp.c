8b154b752d11fad956a42252199d3de8d806037f
Jose manuel Hidalgo romero <jhidalgo@c1r12s2.42madrid.com>
2021-04-12 21:27:51 +0200

    memcmp OK

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: jhidalgo <jhidalgo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 18:07:20 by jhidalgo          #+#    #+#             */
-/*   Updated: 2021/04/07 18:08:37 by jhidalgo         ###   ########.fr       */
+/*   Updated: 2021/04/12 21:27:13 by jhidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const char	*str1;
-	const char	*str2;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
-	str1 = s1;
-	str2 = s2;
-	while ((*str1 && *str2) && n-- > 0)
-	{
-		if (*str1++ != *str2++)
-			return (str1 - str2);
-	}
-	return (str1 - str2);
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (str1[i] == str2[i] && i < n - 1)
+		i++;
+	return (str1[i] - str2[i]);
 }
