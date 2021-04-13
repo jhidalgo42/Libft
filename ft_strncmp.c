595eae40b3d506ce2bfffb0d719094b5265c3eec
Jose manuel Hidalgo romero <jhidalgo@c1r11s2.42madrid.com>
2021-04-13 13:02:46 +0200

    strncmp OK next strlcpy

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jhidalgo <jhidalgo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/06 18:05:55 by jhidalgo          #+#    #+#             */
-/*   Updated: 2021/04/12 19:56:16 by jhidalgo         ###   ########.fr       */
+/*   Updated: 2021/04/13 13:01:31 by jhidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,23 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
-	int		j;
+	size_t			i;
+	int				j;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
 	i = 1;
 	j = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	if (n == 0)
 	{
 		return (0);
 	}
-	while ((s1[j] == s2[j] && i < n) && (s1[j] != '\0' && s2[j] != '\0'))
+	while ((str1[j] == str2[j] && i < n) && (str1[j] != '\0' && str2[j] != '\0'))
 	{
 		i++;
 		j++;
 	}
-	return (s1[j] - s2[j]);
+	return (str1[j] - str2[j]);
 }
